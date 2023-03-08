4dfe5a7ce36f11060b24c5a525636a24ace5c29e
Santhosh <ajanthas1@gmail.com>
2023-03-08 17:01:22 +0530

    Adding Files

---
+++ SnakeAndLadder/com/lxisoft/game/PlayerParamesh.java
@@ -0,0 +1,13 @@
+package com.lxisoft.game;
+import com.lxisoft.game.Player;
+
+public class PlayerParamesh extends Player{
+	
+public Player creatingPlayerParamesh(){
+		
+		Player player = new Player();
+		player.setPlayerName("Paramesh");
+		return player;
+	}
+	
+}
