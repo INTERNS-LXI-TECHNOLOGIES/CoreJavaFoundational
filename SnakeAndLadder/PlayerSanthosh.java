5075be8c6d7b5bfa860f60ff74c12071b3ec4145
Santhosh <ajanthas1@gmail.com>
2023-03-08 16:18:47 +0530

    adding files

---
+++ SnakeAndLadder/PlayerSanthosh.java
@@ -0,0 +1,13 @@
+package com.lxisoft.game;
+import com.lxisoft.game.Player;
+
+public class PlayerSanthosh extends Player{
+	
+public Player creatingPlayerSanthosh(){
+		
+		Player player = new Player();
+		player.setPlayerName("Santhosh");
+		return player;
+	}
+		
+}
