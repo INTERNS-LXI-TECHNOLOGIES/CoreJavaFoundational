574fe9ce93c39610ff6e0b7b6570f2444845258c
nimmyjoseph24 <nimmyjosephvtj@gmail.com>
2024-01-29 21:36:13 +0530

    Hospital project completed

--- hospitalproject/com/lxisoft/hospitalproject/controller/HospitalController.java
+++ hospitalproject/com/lxisoft/hospitalproject/controller/HospitalController.java
@@ -11,8 +11,9 @@ public  class HospitalController {
 
 public void addPatientDetails(){
     Patient patient = new Patient();
-System.out.println("Enter patient's name");
-patient.setPatientName(sc.nextLine());
+    System.out.println("PLEASE PROVIDE PATIENT'S DETAILS..................");
+    System.out.println("Enter patient's name");
+    patient.setPatientName(sc.nextLine());
 
 System.out.println("Enter patient's age");
 patient.setAge(Integer.parseInt(sc.nextLine()));
@@ -32,12 +33,29 @@ patientList.add(patient);
             System.out.println("Decease:" + p.getSick());
 
         }
-
     }
-
-
     public void searchPatient(){
 
-    }
+        System.out.println("Enter Patients Name");
+        String name = sc.nextLine();
+        boolean patientFound = false;
+    for(Patient p : patientList)   
+    {
+        if(p.getPatientName().equals(name)){
+    patientFound = true;
+            System.out.print("...................PATIENT DETAILS..................\n");
+            System.out.println("Patient Name:" + p.getPatientName());
+            System.out.println("Patient Age:"+  p.getAge());
+            System.out.println("Decease:" + p.getSick());
 
-}
+        }
+       
+        }
+        if (!patientFound) {
+            System.out.println("Patient not found");
+        }
+    }
+    
+
+    }
+    
