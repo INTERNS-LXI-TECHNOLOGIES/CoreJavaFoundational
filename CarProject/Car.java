df088320ee232879cb3e2bf7612a0d0cb62972ab
laalu10 <lalu000046@gmail.com>
2024-03-09 05:04:23 +0530

    car project compleated

---
+++ CarProject/Car.java
@@ -0,0 +1,9 @@
+public class Car{
+	
+String name;
+String num;
+String companyname;
+String cc;
+String mileage;
+String price;
+}
---
+++ CarProject/Tdd.java
@@ -0,0 +1,22 @@
+public class Tdd
+{
+	public static void main(String[] args)
+	{
+		Car car=new Car();
+		car.name="HONDA CITY";
+		car.num="KL 51 H 5555";
+		car.companyname="HONDA";
+		car.cc="1300cc";
+		car.mileage="13kpml";
+		car.price="8lakhs";
+		
+		System.out.println(car.name);
+		System.out.println(car.num);
+		System.out.println(car.companyname);
+		System.out.println(car.cc);
+		System.out.println(car.mileage);
+		System.out.println(car.price);
+		
+	}
+}
+		
