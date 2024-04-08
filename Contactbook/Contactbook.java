b3c8fefa548b121fa490c2f724afa7c2180c9efb
Avinashvn <avinashvnair007@gmail.com>
2024-04-08 16:33:04 +0530

    file reader used in contactbook project

--- Contactbook/Contactbook.java
+++ Contactbook/Contactbook.java
@@ -3,13 +3,39 @@ public class Contactbook{
  long mobNumber;
  String mailId;
 
+  public void setName(String name){
+	  this.name = name;
+  }
+  
+  public String getName(){
+	  return this.name;
+  }
+
+  public void setMobNumber(long number){
+	  this.mobNumber = number;
+  }
+
+  public long getMobNumber(){
+	  return this.mobNumber;
+  }
+  
+  public void setMailId(String mailId){
+	  this.mailId = mailId;
+  }
+  
+  public String getMailId(){
+	  return this.mailId;
+  }
+
+  
+
 
 public String toCSVFile(){
 	return name+","+mobNumber+","+mailId+"\n";
 }
 
 public String toString(){
-	return name + "," + mobNumber + "," + mailId + "\n";
+	return getName() + "," + getMobNumber() + "," + getMailId() + "\n";
 }
 
 
--- Contactbook/Main.java
+++ Contactbook/Main.java
@@ -1,26 +1,46 @@
 import java.util.Scanner;
 import java.io.*;
+import java.io.FileReader;
 
 public class Main{
 
 public static void main(String[] args){
 
 Scanner scanner = new Scanner(System.in);
-Contactbook[] contactbook = new Contactbook[2];
+Contactbook[] contactbook = new Contactbook[8];
  System.out.println("Save Contacts");
 for(int i = 0; i < contactbook.length ; i++){
 	 contactbook[i] = new Contactbook();
+}
+     contactbook[0].setName("Ajay");
+	 contactbook[0].setMobNumber(8590521357L);
+	 contactbook[0].setMailId("ajay.krish@gmail.com");
+	  contactbook[1].setName("Arjun");
+	 contactbook[1].setMobNumber(7025054977L);
+	 contactbook[1].setMailId("arjun345@gmail.com");
+	  contactbook[2].setName("Farseena");
+	 contactbook[2].setMobNumber(8289869894L);
+	 contactbook[2].setMailId("farseena@gmail.com");
+	  contactbook[3].setName("Sreeja");
+	 contactbook[3].setMobNumber(9447533989L);
+	 contactbook[3].setMailId("sree.sreeja@gmail.com");
+	  contactbook[4].setName("Vignesh");
+	 contactbook[4].setMobNumber(9446669993L);
+	 contactbook[4].setMailId("vigneshambat@gmail.com");
+
+for(int i = 5;i < contactbook.length ; i++){
 	 System.out.println("Enter Name : ");
-	 contactbook[i].name = scanner.nextLine();
+	 contactbook[i].setName(scanner.nextLine());
 	 System.out.println("Enter Number : ");
-	 contactbook[i].mobNumber = scanner.nextLong();
+	 contactbook[i].setMobNumber(scanner.nextLong());
 	 scanner.nextLine();
 	 System.out.println("Enter Mail ID : ");
-	 contactbook[i].mailId = scanner.nextLine();
+	 contactbook[i].setMailId(scanner.nextLine());
 }
-try{
-File file = new File("E:\\java\\File\\DemoProject.csv");
- 
+
+File file = new File("E:\\java\\File\\Contactbook.csv");
+
+try{ 
 FileWriter fileWriter = new FileWriter(file);
 
 for (Contactbook data : contactbook){
@@ -33,6 +53,18 @@ catch(IOException e){
 	System.out.println( "An eroor occured : "+e.getMessage());
 }
 
+try (FileReader fileReader = new FileReader(file);
+             Scanner scanner1 = new Scanner(fileReader)) {
+
+            scanner.useDelimiter(",|\\n");
+			
+            while (scanner1.hasNext()) {
+                String part = scanner1.next();
+                System.out.println(part); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 }
 
