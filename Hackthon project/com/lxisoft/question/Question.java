948dd2310abf3a9b8278857c411ce6f534edb69b
Ajay <Ajaykrishnachnr@gmail.com>
2024-04-08 16:39:13 +0530

    Hackthon overlod constructor completed

---
+++ Hackthon project/com/lxisoft/question/Question.java
@@ -0,0 +1,16 @@
+package com.lxisoft.question;
+public class Question{
+
+protected String question;
+protected String  correctAnswer;
+
+public Question(String question,String correctAnswer){
+	
+	this.question =question;
+	this.correctAnswer=correctAnswer;
+}
+
+
+
+
+ }
---
+++ Hackthon project/com/lxisoft/question/Tdd.java
@@ -0,0 +1,20 @@
+package com.lxisoft.question;
+import java.util.Scanner;
+
+public class Tdd{
+public static void main(String[] args){
+       Question[] questions = new Question[10];
+       questions[0]= new Question("What is  your Name","Ajay");
+	   questions[1]= new Question("Who is the chief Minister of Kerala","Pinarayi Vijayan");
+	   questions[2]= new Question("	Who is the Prime minister of India?","NarendraModi");
+	   questions[3]= new Question("which is the largest District in Kerala?","Idukki");
+	   questions[4]= new Question("Which is the smallest District in Kerala","Alapuzha");
+	   questions[5]= new Question("Who is the first prime minister of india?" "Nehru","Gandhiji","Bhagath singh","Ajay");
+	   questions[6]= new Question("what is the capital of the India?","NewDelhi","Bangloor","Mysoore",);
+	   questions[7]= new Question("when will get freedom in India","1997","1945","1652",);
+       questions[8] = new Question("What is the capital of France?" "London","Paris","Berlin","Rome");	   
+       question[9]=  new Question("What is the chemical symbol for gold?","Au","Ag","Cu","Fe");
+	   question[10]= new Question("What is the Powerhouse of the Cell","Nucleus","Mitochondria","Ribosome","Golgi apparatus");
+	   
+}
+}
