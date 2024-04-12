b0c81cf5a355dd0c0d488a3777adfdbd56ae143e
ANLAVK <anilasuresh44@gmail.com>
2024-04-12 18:05:10 +0530

    school project is completed

--- school scanner/Tdd.java
+++ school scanner/Tdd.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
+
 public class Tdd{
-	public static void main(String[] args){
-	
-	School school=new School();
+public static void main(String args[]){
+	int i=0;
+	int j=0;
+
+		School school=new School();
 	
 	Scanner scanner = new Scanner(System.in);
 
 	do{
-    System.out.println("Enter student name ");
+	
+    System.out.println("Enter student name: ");
 	school.name = scanner.nextLine();
 	
-	System.out.println("Enter Student class");
+	System.out.println("Enter Student class:");
     school.clas= scanner.nextInt();
 	
-	System.out.println("Enter student age");
+	System.out.println("Enter student age:");
     school.age= scanner.nextInt();
 	
-	System.out.println("Enter student rollno");
+	System.out.println("Enter student rollno:");
     school.rollno= scanner.nextInt();
 	
 	System.out.println("Enter student gender: ");
@@ -27,7 +31,18 @@ public class Tdd{
 	System.out.println("Enter student subject: ");
 	school.subject= scanner.nextLine();
 	
-	for(int i=0;i<5;i++){
+	
+	System.out.println("Do you want to enter another student? (yes/no): ");
+    school.response = scanner.nextLine();
+	
+	
+	 if(!school.response.equalsIgnoreCase("yes")) {
+                break; 
+            }
+		i++;	
+	} while(i<2);	
+
+	do{
 	System.out.println("Do you want to change subject? (yes/no): ");
     school.change = scanner.nextLine(); 
 	
@@ -38,21 +53,8 @@ public class Tdd{
 	System.out.println("Enter new subject: ");
 	school.subject1= scanner.nextLine();		
 	
-	}
-	
-	System.out.println("Do you want to enter another student? (yes/no): ");
-    school.response = scanner.nextLine();
-	
-	
-	 if(!school.response.equalsIgnoreCase("yes")) {
-                break; 
-            }
-		
-	System.out.println("Enter new subject: ");
-	school.subject1= scanner.nextLine();
-	
-	} while (true);
-	
+   j++;
+		} while(j<2);
 	
 	}	
 	
