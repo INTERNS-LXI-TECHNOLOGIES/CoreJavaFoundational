f6f6e31c099bbb9caebf0d84023539b88327a6e6
Mhd-Anas <anuanas563@gmail.com>
2024-04-23 16:27:00 +0530

    Contact book project on processing

--- contactproject/ContactBook.java
+++ contactproject/ContactBook.java
@@ -1,29 +1,77 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.*;
 public class ContactBook{
-	private String name;
-	private long phnNo;
-	private String email;
+private String name;
+private long phnNo;
+private String email;
 	
-	public String getName(){
-		return name;
+ public String getName(){
+	 return name;
 	}
-	public void setName(String name){
-		this.name=name;
+ public void setName(String name){
+	 this.name=name;
 	}
-	public long getPhnNo(){
-		return phnNo;
+ public long getPhnNo(){
+	 return phnNo;
 	}
-	public void setPhnNo(long phnNo){
-		this.phnNo=phnNo;
+ public void setPhnNo(long phnNo){
+	 this.phnNo=phnNo;
 	}
-	public String getEmail(){
-		return email;
+ public String getEmail(){
+	 return email;
 	}
-	public void setEmail(String email){
-		this.email=email;
+ public void setEmail(String email){
+	 this.email=email;
 	}
 	
-	public String toString(){
-		return name+"\n"+phnNo+"\n"+email;
+public void file(ContactBook[] contactBook){
+	
+     String filePath = "C:\\LXI\\csv\\contacts.csv";
+	 try {
+         FileWriter writer = new FileWriter(filePath);
+         writer.write("Name ,Phone Number ,Email \n");
+            for (ContactBook contact : contactBook) {
+                writer.write(contact.csvFile() + "\n");
+            }
+         writer.close();
+         System.out.println("Contacts saved to " + filePath + " successfully!");
+        } 
+     catch (IOException e) {
+            e.printStackTrace();
+        }
+	
+	}
+	   public void updateContact(String newName, long newPhnNo, String newEmail) {
+        this.name = newName;
+        this.phnNo = newPhnNo;
+        this.email = newEmail;
+		
+    }
+	
+    public void readContacts() {
+        String filePath = "C:\\LXI\\csv\\contacts.csv";
+        try {
+            File read = new File(filePath);
+         Scanner myReader = new Scanner(read);  
+		 System.out.println("Contacts:");
+            while (myReader.hasNextLine()) {
+             String data = myReader.nextLine();
+             System.out.println(data);
+            }
+         myReader.close();
+        }
+         catch (IOException e) {
+            System.out.println("No existing contacts.");
+        }
+    }
+	
+	public String csvFile(){
+		
+		//System.out.println("Name,Phone Number,Email");
+		return name+","+phnNo+","+email;
 	}
 
 }
--- contactproject/Tdd.java
+++ contactproject/Tdd.java
@@ -1,31 +1,52 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.*;
 public class Tdd{
 public static void main(String[] agrs){
-
-        Scanner scanner =new Scanner(System.in);
-        ContactBook[] contactBook = new ContactBook[2];
+    Scanner scanner =new Scanner(System.in);
+    ContactBook[] contactBook = new ContactBook[2];
+	
+	ContactBook fileReader = new ContactBook();
+        fileReader.readContacts();
 		
-       for(int i=0; i<contactBook.length;i++){
-		   contactBook[i]=new ContactBook();
-		   System.out.println("Enter name: ");
-		   contactBook[i].setName(scanner.nextLine());
-		   System.out.println("Enter Phone number");
-		   contactBook[i].setPhnNo(scanner.nextLong());
-		   scanner.nextLine();
-		   System.out.println("Enter Email");
-		   contactBook[i].setEmail(scanner.nextLine()); 
-
-		   
-		   
-	   }	   
-	   
-	     for (ContactBook contact : contactBook) {
-            System.out.println(contact);
+		//System.out.println("Contact Book"+"\n");
+		
+    for(int i=0; i<contactBook.length;i++){
+		 contactBook[i]=new ContactBook();
+		 System.out.println("Enter name: ");
+		 contactBook[i].setName(scanner.nextLine());
+		 System.out.println("Enter Phone number");
+		 contactBook[i].setPhnNo(scanner.nextLong());
+		 
+		 scanner.nextLine();
+		 System.out.println("Enter Email");
+		 contactBook[i].setEmail(scanner.nextLine());
+		 System.out.println("\n"+"Saved......."+"\n");
+		 }	
+	System.out.println("Enter the name of the contact to update: ");
+       String nameToUpdate = scanner.nextLine();
+     for (ContactBook contact : contactBook) {
+          if (contact.getName().equals(nameToUpdate)) {
+             System.out.println("Enter new name: ");
+             String newName = scanner.nextLine();
+             System.out.println("Enter new Phone number: ");
+             long newPhnNo = scanner.nextLong();
+             scanner.nextLine(); // consume newline
+             System.out.println("Enter new Email: ");
+             String newEmail = scanner.nextLine();
+             contact.updateContact(newName, newPhnNo, newEmail);
+             System.out.println("Contact updated successfully!");
+              break;
+            }
         }
-	   
-	   
-    }
-}
+	ContactBook fileSaver = new ContactBook();
+	
+	
+	
+	fileSaver.file(contactBook);
+  
+	
+  }  
+} 
